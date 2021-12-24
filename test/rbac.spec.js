8bed10faed700318bf9be663f178708fee06db69
LuoHao <hluo5206@gmail.com>
2021-12-24 22:50:24 +0800

    build(rbac test): test
    rbac test

--- test/rbac.spec.js
+++ test/rbac.spec.js
@@ -3,8 +3,8 @@ const { newEnforcer } = require('casbin');
 
 describe("rbac API 测试", function() {
 
-  const model_path = path.resolve(__dirname, '../src/config/rbac_model.conf');
-  const policy_path = path.resolve(__dirname, '../src/config/rbac_policy.csv');
+  const model_path = path.resolve(__dirname, './rbac_model.conf');
+  const policy_path = path.resolve(__dirname, './rbac_policy.csv');
   
   test("HasRoleForUser 确定用户是否具有角色。", async function() {
     const enforcer = await newEnforcer(model_path, policy_path);
@@ -16,5 +16,4 @@ describe("rbac API 测试", function() {
     await enforcer.addRoleForUser("alice", "data1_admin"); // 在内存中添加不会持久化到文件上
     expect(await enforcer.hasRoleForUser("alice", "data1_admin")).toBeTruthy();
   });
-  
 });
